288714bbeb0767019a13eaf308aeb4ac7bafe761
khskeb05131 <54461238+khskeb05131@users.noreply.github.com>
2019-10-19 12:40:31 +0900

    mv

--- game_basic04/DodgeGame.js
+++ game_basic04/DodgeGame.js
@@ -8,40 +8,36 @@ var GAME_STATE_OVER = 2;  // 게임 오버
 // 게임 상태값을 저장하는 변수
 var GameState = GAME_STATE_READY; // 초깃값은 준비 상태
 
+var intervalID;
+
+var arrMissiles = new Array();
+
 var imgBackground = new Image();
-imgBackground.src = "img/background.png";
+imgBackground.src = "img/Background.png";
 imgBackground.addEventListener("load", drawScreen, false);
 
 var imgPlayer = new Image();
 imgPlayer.src = "img/player.png";
 imgPlayer.addEventListener("load", drawScreen, false);
 
+var imgMissile = new Image();
+imgMissile.src = "img/missile.png";
+
 var intPlayerX = 350;
 var intPlayerY = 250;
 
-var tempMissile1 = {x:0, y:0, go_x:1, go_y:1};
-var tempMissile2 = {x:800, y:0, go_x:-1, go_y:1};
-var tempMissile3 = {x:800, y:600, go_x:-1, go_y:-1};
-var tempMissile4 = {x:0, y:600, go_x:1, go_y:-1};
+var intTime = 0; // 살아남은 시간을 저장할 변수
 
-var imgMissile = new Image();
-imgMissile.src = "img/missile.png"
-
-var intervalID;
-
-function onGameStart(){
-  intervalID = setInterval(MoveMissile, 100);
-}
 function drawScreen()
 {
   var theCanvas = document.getElementById("GameCanvas");
   var Context  = theCanvas.getContext("2d");
 
   Context.fillStyle = "#000000";
-  Context.fillRect(0, 0, 800, 600); 
+  Context.fillRect(0, 0, 800, 600);  
   
   // 배경 화면 그리기
-  Context.drawImage(imgBackground,0,0);
+  Context.drawImage(imgBackground, 0, 0);
   
   // 플레이어 그리기
   Context.drawImage(imgPlayer, intPlayerX, intPlayerY); 
@@ -51,60 +47,36 @@ function drawScreen()
   Context.textBaseline = "top";
   
   // 게임 준비 중
-  if( GameState == GAME_STATE_READY )  {
+  if( GameState == GAME_STATE_READY )
+  {
     Context.fillText( "준비", 330, 180  );
   }
-
   // 게임 중
-  else if( GameState == GAME_STATE_GAME )  {
-    Context.drawImage(imgMissile, tempMissile1.x, tempMissile1.y);
-    Context.drawImage(imgMissile, tempMissile2.x, tempMissile2.y);
-    Context.drawImage(imgMissile, tempMissile3.x, tempMissile3.y);
-    Context.drawImage(imgMissile, tempMissile4.x, tempMissile4.y);
+  else if( GameState == GAME_STATE_GAME )
+  {
+    // 총알을 그려준다
+    for( var i = 0; i < arrMissiles.length; i++ )
+    {
+      Context.drawImage(imgMissile, arrMissiles[i].x, arrMissiles[i].y);  
+    }
   }
   // 게임 오버
-  else if( GameState == GAME_STATE_OVER )  {
-    Context.drawImage(imgMissile, tempMissile1.x, tempMissile1.y);
-    Context.drawImage(imgMissile, tempMissile2.x, tempMissile2.y);
-    Context.drawImage(imgMissile, tempMissile3.x, tempMissile3.y);
-    Context.drawImage(imgMissile, tempMissile4.x, tempMissile4.y);
+  else if( GameState == GAME_STATE_OVER )
+  {
+    // 총알을 그려준다
+    for( var i = 0; i < arrMissiles.length; i++ )
+    {
+      Context.drawImage(imgMissile, arrMissiles[i].x, arrMissiles[i].y);  
+    }
     Context.fillText( "게임 오버", 330, 180  );   
-  }  
+  }
+  
+  Context.font = '20px Arial'; 
+  Context.fillText( "Time : " + intTime / 1000, 20, 5  ); 
+  Context.fillText( "총알 수 : " + arrMissiles.length , 680, 5  ); 
+  
 }
 
-
-function MoveMissile()
-{
-  tempMissile1.x += tempMissile1.go_x * 3;
-  tempMissile1.y += tempMissile1.go_y * 3;
-  if(IsCollisionWithPlayer(tempMissile1.x, tempMissile1.y)){
-    GameState = GAME_STATE_OVER;
-    clearInterval(intervalID);
-  }
-
-  tempMissile2.x += tempMissile2.go_x * 3;
-  tempMissile2.y += tempMissile2.go_y * 3;
-  if(IsCollisionWithPlayer(tempMissile2.x, tempMissile2.y)){
-    GameState = GAME_STATE_OVER;
-    clearInterval(intervalID);
-  } 
-
-  tempMissile3.x += tempMissile3.go_x * 3;
-  tempMissile3.y += tempMissile3.go_y * 3;
-  if(IsCollisionWithPlayer(tempMissile3.x, tempMissile3.y)){
-    GameState = GAME_STATE_OVER;
-    clearInterval(intervalID);
-  }
-
-  tempMissile4.x += tempMissile4.go_x * 3;
-  tempMissile4.y += tempMissile4.go_y * 3;
-  if(IsCollisionWithPlayer(tempMissile4.x, tempMissile4.y)){
-    GameState = GAME_STATE_OVER;
-    clearInterval(intervalID);
-  }
-
-  //화면 갱신  drawScreen();
-}
 
 function onkeydown(e) 
 {
@@ -115,50 +87,167 @@ function onkeydown(e)
     if( e.keyCode == 13 )
     {
       // 엔터를 입력하면 게임시작
-      GameState = GAME_STATE_GAME;
       onGameStart();
+
     }
   }
   // 게임 중
   else if( GameState == GAME_STATE_GAME )
   {
-    
     // 기존의 플레이어 이동 처리 코드
-    switch( e.keyCode )    {
+    switch( e.keyCode )
+    {
       case 37: // LEFT
         intPlayerX-=5;
-        if( intPlayerX < 0 ) {
+        if( intPlayerX < 0 )
+        {
           intPlayerX = 0;
-        } 
+        }
       break;
       case 39: // RIGHT
         intPlayerX+=5;
-        if( intPlayerX > 740 ) {
+        if( intPlayerX > 740 )
+        {
           intPlayerX = 740;
-        }       
+        }     
       break;
       case 38: // UP
         intPlayerY-=5;
-        if( intPlayerY < 0 ) {
+        if( intPlayerY < 0 )
+        {
           intPlayerY = 0;
-        } 
+        }
       break;
       case 40: // DOWN
         intPlayerY+=5;
-        if( intPlayerY > 540 ) {
+        if( intPlayerY > 540 )
+        {
           intPlayerY = 540;
-        }   
+        } 
       break;
     };
   }
   // 게임 오버
-  else if( GameState == GAME_STATE_OVER )  {
+  else if( GameState == GAME_STATE_OVER )
+  {
     // 게임 준비 상태로 변경
-    if( e.keyCode == 13 )    {
+    if( e.keyCode == 13 )
+    {
       // 엔터를 입력하면 준비 상태로
-      GameState = GAME_STATE_READY;
+      onReady();
     }
-  }  
+  }
+  
   // 화면 갱신
   drawScreen();
 }
+
+function RandomNextInt ( max ) {
+  return 1 + Math.floor( Math.random() * max );
+}
+
+function onGameStart() 
+{
+  // 게임 시작
+  GameState = GAME_STATE_GAME;
+  intervalID = setInterval( InGameUpdate, 100 );
+  
+  // 총알 위치 추가하기
+  
+  arrMissiles.push({x:0, y:0, go_x:1, go_y:1});
+  arrMissiles.push({x:800, y:0, go_x:-1, go_y:1});
+  arrMissiles.push({x:800, y:600, go_x:-1, go_y:-1});
+  arrMissiles.push({x:0, y:600, go_x:1, go_y:-1});
+  
+}
+function onGameOver() 
+{
+  // 게임 오버
+  GameState = GAME_STATE_OVER;
+  clearInterval( intervalID );
+}
+function onReady() 
+{
+  
+  GameState = GAME_STATE_READY;
+  intPlayerX = 350;
+  intPlayerY = 250;
+  while(arrMissiles.length != 0)  {
+    arrMissiles.pop();
+  }
+}
+
+function InGameUpdate () 
+{
+  // 시간 체크
+  intTime += 100;
+  if( intTime % 5000  == 0 )
+  {
+    for( var i = 0; i < 5; i++)
+    {
+      var MissileType = RandomNextInt(4);
+      var intX, intY, intGoX, intGoY;
+      switch( MissileType )
+      {
+        case 1: // 왼쪽 총알
+          intX = 0;
+          intY = RandomNextInt(600);
+          intGoX = RandomNextInt(2);
+          intGoY = -2 + RandomNextInt(4);
+          break;
+        case 2: // 오른쪽 총알
+          intX = 800;
+          intY = RandomNextInt(600);
+          intGoX = -RandomNextInt(2);
+          intGoY = -2 + RandomNextInt(4);
+          break;
+        case 3: // 위쪽 총알
+          intX = RandomNextInt(800);
+          intY = 0;
+          intGoX = -2 + RandomNextInt(4);
+          intGoY = RandomNextInt(2);
+          break;
+        case 4: // 아래쪽 총알
+          intX = RandomNextInt(800);
+          intY = 600;
+          intGoX = -2 + RandomNextInt(4);
+          intGoY = -RandomNextInt(2);
+          break;
+      };
+      
+      arrMissiles.push( {x: intX, y: intY, go_x: intGoX, go_y: intGoY} );
+    }
+  } 
+  
+  // 총알 이동 처리 
+  MoveMissile();
+}
+
+function MoveMissile() 
+{
+  //총알 이동 처리
+  for(var i =0; i < arrMissiles.length; i++){
+    arrMissiles[i].x += arrMissiles[i].go_x * 3;
+    arrMissiles[i].y += arrMissiles[i].go_y * 3;
+    if(IsCollisionWithPlayer(arrMissiles[i].x, arrMissiles[i].y)){
+      onGameOver();
+    }
+  }
+  //화면 갱신
+  drawScreen();
+}
+
+function IsCollisionWithPlayer( x, y )
+{
+  if( intPlayerX + 45 >  x + 5  && 
+    intPlayerX + 15  < x + 25 && 
+    intPlayerY + 15  < y + 25  && 
+    intPlayerY + 45  > y + 5 )
+  {
+    return true;
+  }
+    
+  return false;
+}
+
+
